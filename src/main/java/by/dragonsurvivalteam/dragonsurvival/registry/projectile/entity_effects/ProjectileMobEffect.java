d5257777d278f090f079e6550d065fd8e94ab8d9
seelder <zlschect@gmail.com>
2024-12-24 16:21:01 -0500

    Added language handling for projectile mob effect

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/projectile/entity_effects/ProjectileMobEffect.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/projectile/entity_effects/ProjectileMobEffect.java
@@ -1,16 +1,27 @@
 package by.dragonsurvivalteam.dragonsurvival.registry.projectile.entity_effects;
 
+import by.dragonsurvivalteam.dragonsurvival.registry.datagen.lang.LangKey;
+import by.dragonsurvivalteam.dragonsurvival.util.DSColors;
+import by.dragonsurvivalteam.dragonsurvival.util.Functions;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.RegistryCodecs;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.enchantment.LevelBasedValue;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public record ProjectileMobEffect(HolderSet<MobEffect> effects, LevelBasedValue amplifier, LevelBasedValue duration, LevelBasedValue probability) implements ProjectileEntityEffect {
     public static final MapCodec<ProjectileMobEffect> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
@@ -29,6 +40,32 @@ public record ProjectileMobEffect(HolderSet<MobEffect> effects, LevelBasedValue 
                 }
             });
         }
+    }
+
+    @Override
+    public List<MutableComponent> getDescription(final Player dragon, final int level) {
+        List<MutableComponent> components = new ArrayList<>();
+        double duration = Functions.ticksToSeconds((int) duration().calculate(level));
+
+        for (Holder<MobEffect> effect : effects) {
+            MutableComponent name = Component.literal("§6■ ").append(Component.translatable(LangKey.ABILITY_APPLIES).append(Component.translatable(effect.value().getDescriptionId())).withColor(DSColors.ORANGE));
+            int amplifier = (int) amplifier().calculate(level);
+
+            if (amplifier > 0) {
+                name.append(Component.literal(Integer.toString(amplifier)).withColor(DSColors.ORANGE));
+            }
+
+            name.append(Component.translatable(LangKey.ABILITY_EFFECT_DURATION, DSColors.blue(duration)));
+            float probability = probability().calculate(level);
+
+            if (probability < 1) {
+                name.append(Component.translatable(LangKey.ABILITY_EFFECT_CHANCE, DSColors.blue(NumberFormat.getPercentInstance().format(probability))));
+            }
+
+            components.add(name);
+        }
+
+        return components;
     }
 
     @Override
