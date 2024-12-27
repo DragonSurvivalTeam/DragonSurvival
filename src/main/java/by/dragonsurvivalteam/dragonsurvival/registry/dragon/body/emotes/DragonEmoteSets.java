e3996ee7d21dff16ae1ebe13bd46b704abad2c9c
BlackAures1 <dragonsurvival@mail.ru>
2024-12-28 00:34:28 +0100

    WIP emotions

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/body/emotes/DragonEmoteSets.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/body/emotes/DragonEmoteSets.java
@@ -11,8 +11,42 @@ import java.util.List;
 public class DragonEmoteSets {
     public static final ResourceKey<DragonEmoteSet> DEFAULT_EMOTES = key("default_emotes");
 
-    @Translation(type = Translation.Type.EMOTE, comments = "Angry")
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - Fix Head and Tail")
+    public static final String BLEND_HEAD_LOCKED = "head_turn";
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - Angry")
     public static final String BLEND_ANGRY = "blend_angry";
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - Charge")
+    public static final String BLEND_SPELL_CHARGE = "spell_charge";
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - Sad")
+    public static final String BLEND_SAD = "blend_sad";
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - Blep ")
+    public static final String BLEND_BLEP = "blend_blep";
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - Tongue")
+    public static final String BLEND_MAW = "blend_maw";
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - Lick")
+    public static final String BLEND_LICKING = "blend_licking";
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - Yaw")
+    public static final String BLEND_YAWNING = "blend_yawning";
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - Sniff")
+    public static final String BLEND_SNIFFING = "blend_sniffing";
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - One Bite")
+    public static final String BLEND_SPELL_SHOT = "spell_shot";
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - Bites")
+    public static final String BLEND_BITE = "bite";
+
+    @Translation(type = Translation.Type.EMOTE, comments = "§6Blend - Open Mouth")
+    public static final String BLEND_BREATH= "breath";
 
     @Translation(type = Translation.Type.EMOTE, comments = "Sitting Curled")
     public static final String SITTING_CURLED = "sitting_curled_tail_left";
@@ -27,9 +61,20 @@ public class DragonEmoteSets {
         context.register(DEFAULT_EMOTES,
                 new DragonEmoteSet(
                         List.of(
-                                DragonEmote.Builder.of(BLEND_ANGRY).loops().blend().build(),
-                                DragonEmote.Builder.of(SITTING_CURLED).loops().build(),
-                                DragonEmote.Builder.of(SITTING_CURLED, SITTING_CURLED_HEAD_LOCKED).loops().locksHead().locksTail().build(),
+                                DragonEmote.Builder.of(BLEND_HEAD_LOCKED).blend().locksHead().locksTail().build(),
+                                DragonEmote.Builder.of(BLEND_ANGRY).blend().build(),
+                                DragonEmote.Builder.of(BLEND_SPELL_CHARGE).blend().build(),
+                                DragonEmote.Builder.of(BLEND_SAD).blend().build(),
+                                DragonEmote.Builder.of(BLEND_BLEP).blend().build(),
+                                DragonEmote.Builder.of(BLEND_MAW).blend().build(),
+                                DragonEmote.Builder.of(BLEND_LICKING).loops().blend().build(),
+                                DragonEmote.Builder.of(BLEND_YAWNING).loops().blend().build(),
+                                DragonEmote.Builder.of(BLEND_SNIFFING).loops().blend().build(),
+                                DragonEmote.Builder.of(BLEND_SPELL_SHOT).blend().build(),
+                                DragonEmote.Builder.of(BLEND_BITE).loops().blend().build(),
+                                DragonEmote.Builder.of(BLEND_BREATH).blend().build(),
+                                DragonEmote.Builder.of(SITTING_CURLED).build(),
+                                DragonEmote.Builder.of(SITTING_CURLED, SITTING_CURLED_HEAD_LOCKED).locksHead().locksTail().build(),
                                 DragonEmote.Builder.of(SITTING_CURLED, SITTING_CURLED_NO_LOOP).build()
                         )
                 )
