cb89bd9cd2d73dc8cefc384cb5f154cf240c95d5
Seelder <zlschect@gmail.com>
2025-01-04 15:03:00 -0800

    Missing getData call

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/attachments/HarvestBonuses.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/attachments/HarvestBonuses.java
@@ -16,6 +16,11 @@ import org.jetbrains.annotations.NotNull;
 
 @EventBusSubscriber // Only relevant for Players since the harvest events are only fired for them
 public class HarvestBonuses extends Storage<HarvestBonus.Instance> {
+
+    public static HarvestBonuses getData(final Player player) {
+        return player.getData(DSDataAttachments.HARVEST_BONUSES);
+    }
+
     public int getHarvestBonus(final BlockState state) {
         if (storage == null) {
             return HarvestBonus.NO_BONUS_VALUE;
