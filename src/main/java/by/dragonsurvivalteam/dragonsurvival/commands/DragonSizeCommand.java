23e25ae1778dc712e669c9a4a8c633433f7ca915
Seelder <zlschect@gmail.com>
2025-01-09 22:44:05 -0800

    Refactored dragon-size command

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/commands/DragonSizeCommand.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/commands/DragonSizeCommand.java
@@ -1,10 +1,13 @@
 package by.dragonsurvivalteam.dragonsurvival.commands;
 
+import by.dragonsurvivalteam.dragonsurvival.commands.arguments.DragonSizeArgument;
 import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateHandler;
 import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateProvider;
 import by.dragonsurvivalteam.dragonsurvival.registry.datagen.Translation;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
@@ -12,31 +15,50 @@ import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import java.util.Collection;
 
 public class DragonSizeCommand {
-    @Translation(comments = "Set %s size to %s.")
-    private static final String SET_SIZE_TO_PLAYERS = Translation.Type.COMMAND.wrap("ability.clear_from_entities");
+    @Translation(comments = "Set size to %s for %s players.")
+    private static final String SET_SIZE_TO_PLAYERS = Translation.Type.COMMAND.wrap("size.clear_from_entities");
 
-    @Translation(comments = "Set %s size to %s.")
-    private static final String SET_SIZE_TO_PLAYER = Translation.Type.COMMAND.wrap("ability.clear_from_single_entity");
+    @Translation(comments = "Set size to %s for %s.")
+    private static final String SET_SIZE_TO_PLAYER = Translation.Type.COMMAND.wrap("size.clear_from_single_entity");
+
+    @Translation(comments = "Failed to set size since player was not a dragon.")
+    private static final String FAILED_TO_SET_SIZE = Translation.Type.COMMAND.wrap("size.failed_to_set_size");
 
     public static void register(final RegisterCommandsEvent event) {
         event.getDispatcher().register(
                 Commands.literal("dragon-size")
-                        .requires(sourceStack -> sourceStack.hasPermission(2))
+                        .requires(sourceStack -> sourceStack.hasPermission(Commands.LEVEL_GAMEMASTERS))
                         .then(
                                 Commands.argument("targets", EntityArgument.players())
-                                        .executes(sourceStackCommandContext -> {
-                                            Collection<? extends Player> players = EntityArgument.getPlayers(sourceStackCommandContext, "targets");
-                                            for(Player player : players) {
-                                                ServerPlayer serverPlayer = (ServerPlayer) player;
-                                                DragonStateHandler handler = DragonStateProvider.getData(serverPlayer);
+                                        .then(Commands.argument(DragonSizeArgument.ID, new DragonSizeArgument(event.getBuildContext()))
+                                                        .executes(sourceStackCommandContext -> {
+                                                            Collection<? extends Player> players = EntityArgument.getPlayers(sourceStackCommandContext, "targets");
+                                                            double size = DragonSizeArgument.get(sourceStackCommandContext);
+                                                            int count = 0;
+                                                            for(Player player : players) {
+                                                                ServerPlayer serverPlayer = (ServerPlayer) player;
+                                                                DragonStateHandler handler = DragonStateProvider.getData(serverPlayer);
 
-                                                if (handler.isDragon()) {
-                                                    handler.setDesiredSize(serverPlayer, 1.0);
-                                                }
-                                            }
+                                                                if (handler.isDragon()) {
+                                                                    handler.setDesiredSize(serverPlayer, size);
+                                                                    count++;
+                                                                }
+                                                            }
 
-                                            return 1;
-                                        })
+                                                            int finalCount = count;
+                                                            if (players.size() == 1) {
+                                                                if(count == 0) {
+                                                                    sourceStackCommandContext.getSource().sendFailure(Component.translatable(FAILED_TO_SET_SIZE));
+                                                                } else {
+                                                                    sourceStackCommandContext.getSource().sendSuccess(() -> Component.translatable(SET_SIZE_TO_PLAYER, size, players.stream().findFirst().get().getDisplayName()), true);
+                                                                }
+                                                            } else {
+                                                                sourceStackCommandContext.getSource().sendSuccess(() -> Component.translatable(SET_SIZE_TO_PLAYERS, size, finalCount), true);
+                                                            }
+
+                                                            return 1;
+                                                        })
+                                        )
                         )
         );
     }
