b8a234826ea83efd3e0b4e656744abc039f7c8f6
Seelder <zlschect@gmail.com>
2025-01-12 12:40:40 -0800

    Fixed missing TickData in OxygenBonuses

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/attachments/OxygenBonuses.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/attachments/OxygenBonuses.java
@@ -1,15 +1,19 @@
 package by.dragonsurvivalteam.dragonsurvival.registry.attachments;
 
 import by.dragonsurvivalteam.dragonsurvival.common.codecs.OxygenBonus;
-import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.player.Player;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.attachment.AttachmentType;
+import net.neoforged.neoforge.event.tick.EntityTickEvent;
 import net.neoforged.neoforge.fluids.FluidType;
 import org.jetbrains.annotations.NotNull;
 
+@EventBusSubscriber
 public class OxygenBonuses extends Storage<OxygenBonus.Instance> {
     public float getOxygenBonus(final ResourceKey<FluidType> fluidTypeKey) {
         if (storage == null) {
@@ -23,6 +27,19 @@ public class OxygenBonuses extends Storage<OxygenBonus.Instance> {
         }
 
         return bonus;
+    }
+
+    @SubscribeEvent
+    public static void tickData(final EntityTickEvent.Post event) {
+        if (event.getEntity() instanceof Player player) {
+            player.getExistingData(DSDataAttachments.OXYGEN_BONUSES).ifPresent(storage -> {
+                storage.tick(event.getEntity());
+
+                if (storage.isEmpty()) {
+                    player.removeData(DSDataAttachments.OXYGEN_BONUSES);
+                }
+            });
+        }
     }
 
     @Override
