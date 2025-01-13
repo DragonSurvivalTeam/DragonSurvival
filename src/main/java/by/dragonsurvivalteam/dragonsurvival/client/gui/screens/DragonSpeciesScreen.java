e82f1ac1b8b7d338ef9ffcf68096ba0791c1b9e9
Seelder <zlschect@gmail.com>
2025-01-12 22:20:32 -0800

    Made dragon species screen banner change on hover

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/client/gui/screens/DragonSpeciesScreen.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/client/gui/screens/DragonSpeciesScreen.java
@@ -207,12 +207,13 @@ public class DragonSpeciesScreen extends Screen {
 
             @Override
             public void renderWidget(@NotNull final GuiGraphics graphics, int mouseX, int mouseY, float partialTick) {
-                graphics.blit(data.species().value().miscResources().altarBanner(), getX(), getY(), 0, 0, 49, 147, 49, 294);
-
                 if (isHovered() && isTop(mouseY)) {
+                    graphics.blit(data.species().value().miscResources().altarBanner(), getX(), getY(), 0, 0, 49, 147, 49, 294);
                     List<Either<FormattedText, TooltipComponent>> components = new ArrayList<>();
                     components.addFirst(Either.left(Component.translatable(Translation.Type.DRAGON_SPECIES_DESCRIPTION_NO_DIET.wrap(dragonSpecies.getKey().location()))));
                     graphics.renderComponentTooltipFromElements(Minecraft.getInstance().font, components, mouseX, mouseY, ItemStack.EMPTY);
+                } else {
+                    graphics.blit(data.species().value().miscResources().altarBanner(), getX(), getY(), 0, 147, 49, 147, 49, 294);
                 }
             }
         };
