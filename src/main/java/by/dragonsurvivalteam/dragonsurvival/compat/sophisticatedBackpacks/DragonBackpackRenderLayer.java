6e44d327be74d8a1f1bab40201660ff62fd12920
Nik77x <niktornado77@gmail.com>
2025-01-27 12:42:53 +0100

    Rename variables
    Config variables didn't match the naming scheme used elsewhere

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/compat/sophisticatedBackpacks/DragonBackpackRenderLayer.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/compat/sophisticatedBackpacks/DragonBackpackRenderLayer.java
@@ -26,42 +26,46 @@ import java.lang.Math;
 
 public class DragonBackpackRenderLayer extends GeoRenderLayer<DragonEntity> {
 
+    @Translation(key = "render_backpack", type = Translation.Type.CONFIGURATION, comments = "enable / disable backpack rendering")
+    @ConfigOption(side = ConfigSide.CLIENT, category = {"compat", "sophisticated backpacks"}, key = "render_backpack")
+    public static Boolean renderBackpack = true;
+    
     @Translation(key = "backpack_offset_x", type = Translation.Type.CONFIGURATION, comments = "Backpack X offset")
     @ConfigOption(side = ConfigSide.CLIENT, category = {"compat", "sophisticated backpacks"}, key = "backpack_offset_x")
     @ConfigRange(min = -1000, max = 1000)
-    public static double model_offset_x = 0;
+    public static double modelOffsetX = 0;
 
     @Translation(key = "backpack_offset_y", type = Translation.Type.CONFIGURATION, comments = "Backpack Y offset")
     @ConfigOption(side = ConfigSide.CLIENT, category = {"compat", "sophisticated backpacks"}, key = "backpack_offset_y")
     @ConfigRange(min = -1000, max = 1000)
-    public static double model_offset_y = 0.65;
+    public static double modelOffsetY = 0.65;
 
     @Translation(key = "backpack_offset_z", type = Translation.Type.CONFIGURATION, comments = "Backpack Z offset")
     @ConfigOption(side = ConfigSide.CLIENT, category = {"compat", "sophisticated backpacks"}, key = "backpack_offset_z")
     @ConfigRange(min = -1000, max = 1000)
-    public static double model_offset_z = -0.3;
+    public static double modelOffsetZ = -0.3;
 
 
     @Translation(key = "backpack_rot_x", type = Translation.Type.CONFIGURATION, comments = "Backpack X rotation")
     @ConfigOption(side = ConfigSide.CLIENT, category = {"compat", "sophisticated backpacks"}, key = "backpack_rot_x")
     @ConfigRange(min = -180, max = 180)
-    public static double model_rot_x = 90;
+    public static double modelRotX = 90;
 
     @Translation(key = "backpack_rot_y", type = Translation.Type.CONFIGURATION, comments = "Backpack Y rotation")
     @ConfigOption(side = ConfigSide.CLIENT, category = {"compat", "sophisticated backpacks"}, key = "backpack_rot_y")
     @ConfigRange(min = -180, max = 180)
-    public static double model_rot_y = 0;
+    public static double modelRotY = 0;
 
     @Translation(key = "backpack_rot_z", type = Translation.Type.CONFIGURATION, comments = "Backpack Z rotation")
     @ConfigOption(side = ConfigSide.CLIENT, category = {"compat", "sophisticated backpacks"}, key = "backpack_rot_z")
     @ConfigRange(min = -180, max = 180)
-    public static double model_rot_z = 0;
+    public static double modelRotZ = 0;
     
 
     @Translation(key = "backpack_scale", type = Translation.Type.CONFIGURATION, comments = "Backpack scale")
     @ConfigOption(side = ConfigSide.CLIENT, category = {"compat", "sophisticated backpacks"}, key = "backpack_scale")
     @ConfigRange(min = 0.01, max = 10000)
-    public static double model_scale = 0.9;
+    public static double modelScale = 0.9;
 
 
     public DragonBackpackRenderLayer(GeoEntityRenderer<DragonEntity> renderer) {
@@ -72,7 +76,8 @@ public class DragonBackpackRenderLayer extends GeoRenderLayer<DragonEntity> {
     @Override
     public void renderForBone(PoseStack poseStack, DragonEntity animatable, GeoBone bone, RenderType renderType,
                               MultiBufferSource bufferSource, VertexConsumer buffer, float partialTick, int packedLight, int packedOverlay) {
-
+        
+        if (!renderBackpack) return;
 
         if(!bone.getName().equalsIgnoreCase("torso")) return;
 
@@ -107,11 +112,11 @@ public class DragonBackpackRenderLayer extends GeoRenderLayer<DragonEntity> {
 
         Quaternionf rotation = new Quaternionf();
 
-        rotation.rotateX((float) Math.toRadians(model_rot_x));
-        rotation.rotateY((float) Math.toRadians(model_rot_y));
-        rotation.rotateZ((float) Math.toRadians(model_rot_z));
-        poseStack.translate(model_offset_x, model_offset_y, model_offset_z);
-        float s = (float) model_scale;
+        rotation.rotateX((float) Math.toRadians(modelRotX));
+        rotation.rotateY((float) Math.toRadians(modelRotY));
+        rotation.rotateZ((float) Math.toRadians(modelRotZ));
+        poseStack.translate(modelOffsetX, modelOffsetY, modelOffsetZ);
+        float s = (float) modelScale;
         poseStack.scale(s, s, s);
         
         poseStack.rotateAround(rotation, 0, 1, 0);
