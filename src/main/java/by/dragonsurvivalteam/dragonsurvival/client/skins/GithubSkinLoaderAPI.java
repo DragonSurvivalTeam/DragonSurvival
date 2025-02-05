afe545c39928afa3be0d620664b8a3b44af01745
Psither <97207971+Psither@users.noreply.github.com>
2025-02-05 16:40:34 -0500

    Updated dragon skin URL

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/client/skins/GithubSkinLoaderAPI.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/client/skins/GithubSkinLoaderAPI.java
@@ -16,7 +16,7 @@ import java.util.Base64;
 import java.util.Collection;
 
 public class GithubSkinLoaderAPI extends NetSkinLoader {
-    public static final String SKINS_LIST_LINK = "https://api.github.com/repositories/280658566/git/trees/6597d654e16568d2d9e2cdf0dc372041cb081c35?ref=master";
+    public static final String SKINS_LIST_LINK = "https://api.github.com/repos/DragonSurvivalTeam/DragonSurvival/git/trees/30268eb7e0b3ae65e803b0c71437b6a1f85901ff?ref=master";
     private static final String SKINS_PING = "https://raw.githubusercontent.com/DragonSurvivalTeam/DragonSurvival/master/README.md";
 
     private static class SkinFileMetaInfo {
