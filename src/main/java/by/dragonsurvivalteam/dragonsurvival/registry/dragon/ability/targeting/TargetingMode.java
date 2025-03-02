d6ecc800fd772acc1313abc4df12e0ff3ba90a03
BlackAures1 <dragonsurvival@mail.ru>
2025-03-02 15:28:45 +0100

    targeting mode change, dark gui texture pack

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/targeting/TargetingMode.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/targeting/TargetingMode.java
@@ -45,7 +45,7 @@ public enum TargetingMode implements StringRepresentable {
             "4: Enabled Friendly fire on a team no longer flags players as 'enemy'"
     })
     @ConfigOption(side = ConfigSide.SERVER, category = "abilities", key = "player_targeting_handling")
-    public static int PLAYER_FLAG;
+    public static int PLAYER_FLAG = 1;
 
     public static final Codec<TargetingMode> CODEC = StringRepresentable.fromEnum(TargetingMode::values);
     private final String name;
