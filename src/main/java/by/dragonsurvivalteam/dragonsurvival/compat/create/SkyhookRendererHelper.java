fea58fcf85c11293a54c62a51e39dae1fa23fd89
Seelder <zlschect@gmail.com>
2025-03-03 00:03:56 -0800

    Null check for isPlayerRidingSkyhook if the mixin fails someday

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/compat/create/SkyhookRendererHelper.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/compat/create/SkyhookRendererHelper.java
@@ -3,12 +3,14 @@ package by.dragonsurvivalteam.dragonsurvival.compat.create;
 import by.dragonsurvivalteam.dragonsurvival.compat.Compat;
 import by.dragonsurvivalteam.dragonsurvival.mixins.create.PlayerSkyhookRendererAccessor;
 
+import java.util.Set;
 import java.util.UUID;
 
 public class SkyhookRendererHelper {
     public static boolean isPlayerRidingSkyhook(UUID uuid) {
         if (Compat.isModLoaded(Compat.CREATE)) {
-            return PlayerSkyhookRendererAccessor.dragonSurvival$getHangingPlayers().contains(uuid);
+            Set<UUID> hangingPlayers = PlayerSkyhookRendererAccessor.dragonSurvival$getHangingPlayers();
+            return hangingPlayers != null && hangingPlayers.contains(uuid);
         } else {
             return false;
         }
