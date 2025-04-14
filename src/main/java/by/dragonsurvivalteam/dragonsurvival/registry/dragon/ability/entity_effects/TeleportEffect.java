3e6fa5c2f86507e769a40d439428824ab26845be
Psither <97207971+Psither@users.noreply.github.com>
2025-04-13 21:20:06 -0400

    directional teleport

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/entity_effects/TeleportEffect.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/entity_effects/TeleportEffect.java
@@ -11,6 +11,7 @@ import net.minecraft.world.item.enchantment.LevelBasedValue;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.portal.DimensionTransition;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 
 public record TeleportEffect(
         TargetDirection targetDirection,
@@ -24,23 +25,35 @@ public record TeleportEffect(
     @Override
     public void apply(final ServerPlayer dragon, final DragonAbilityInstance ability, final Entity target) {
         if (target.level() instanceof ServerLevel serverLevel && serverLevel.isLoaded(target.getOnPos())) {
-            if (targetDirection.direction().left().orElse(null) == TargetDirection.Type.LOOKING_AT) {
+            if (targetDirection.direction().left().orElse(null) == TargetDirection.Type.TOWARDS_ENTITY) {
+                dragon.changeDimension(
+                        new DimensionTransition(
+                                serverLevel, target.getPosition(0), target.getDeltaMovement(), target.getYRot(), target.getXRot(), DimensionTransition.DO_NOTHING
+                        )
+                );
+            }
+            Vec3 direction = null;
+
+            if (targetDirection.direction().right().isPresent()) {
+                direction = new Vec3(targetDirection.direction().right().get().step()).scale(this.maxDistance.calculate(ability.level())).add(target.getEyePosition());
+            } else if (targetDirection.direction().left().orElse(null) == TargetDirection.Type.LOOKING_AT) {
+                direction = dragon.getLookAngle().scale(this.maxDistance.calculate(ability.level())).add(dragon.getEyePosition());
+            }
+
+            if (direction != null) {
                 BlockHitResult res = target.level().clip(new ClipContext(
                         dragon.getEyePosition(),
-                        dragon.getLookAngle().scale(this.maxDistance.calculate(ability.level())).add(dragon.getEyePosition()),
+                        direction,
                         ClipContext.Block.COLLIDER,
                         ClipContext.Fluid.NONE,
                         dragon));
 
+                // Displace the teleport point back 1 block to avoid teleporting entities into blocks
+                Vec3 destination = new Vec3(res.getLocation().toVector3f()).add(new Vec3(res.getDirection().step()));
+
                 target.changeDimension(
                         new DimensionTransition(
-                                serverLevel, res.getLocation(), target.getDeltaMovement(), target.getYRot(), target.getXRot(), DimensionTransition.DO_NOTHING
-                        )
-                );
-            } else if (targetDirection.direction().left().orElse(null) == TargetDirection.Type.TOWARDS_ENTITY) {
-                dragon.changeDimension(
-                        new DimensionTransition(
-                                serverLevel, target.getPosition(0), target.getDeltaMovement(), target.getYRot(), target.getXRot(), DimensionTransition.DO_NOTHING
+                                serverLevel, destination, target.getDeltaMovement(), target.getYRot(), target.getXRot(), DimensionTransition.DO_NOTHING
                         )
                 );
             }
