7e468712db2cb4bf033d7276de43d50f92f03666
Psither <97207971+Psither@users.noreply.github.com>
2025-04-18 01:13:10 -0400

    Added ability description generation to teleport and hunger effects

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/entity_effects/HungerEffect.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/entity_effects/HungerEffect.java
@@ -1,13 +1,19 @@
 package by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.entity_effects;
 
+import by.dragonsurvivalteam.dragonsurvival.registry.datagen.Translation;
 import by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.DragonAbilityInstance;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodData;
 import net.minecraft.world.item.enchantment.LevelBasedValue;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public record HungerEffect(
@@ -16,6 +22,21 @@ public record HungerEffect(
     Optional<LevelBasedValue> maxSaturation,
     Optional<LevelBasedValue> conversionRate
 ) implements AbilityEntityEffect {
+    @Translation(comments = "Adds %s food.")
+    public static final String ADD_FOOD = Translation.Type.GUI.wrap("hunger.food");
+
+    @Translation(comments = "Adds %s exhaustion.")
+    public static final String ADD_EXHAUSTION = Translation.Type.GUI.wrap("hunger.exhaustion");
+
+    @Translation(comments = "Adds %s saturation.")
+    public static final String SATURATION = Translation.Type.GUI.wrap("hunger.saturation");
+
+    @Translation(comments = "Converts %s excess hunger to saturation.")
+    public static final String CONVERSION_RATE = Translation.Type.GUI.wrap("hunger.conversion_rate");
+
+    @Translation(comments = "You cannot have more than %s saturation from this ability.")
+    public static final String MAX_SATURATION = Translation.Type.GUI.wrap("hunger.max_saturation");
+
     public static final MapCodec<HungerEffect> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
             LevelBasedValue.CODEC.optionalFieldOf("hunger_gain").forGetter(HungerEffect::hunger),
             LevelBasedValue.CODEC.optionalFieldOf("saturation_gain").forGetter(HungerEffect::saturation),
@@ -45,7 +66,7 @@ public record HungerEffect(
     }
 
     private void fillSaturationToCap(FoodData foodData, final DragonAbilityInstance ability, final float saturation) {
-        if (maxSaturation.isPresent()) {
+        if (maxSaturation.isPresent() && saturation > 0) {
             float cap = maxSaturation.get().calculate(ability.level());
             if (foodData.getSaturationLevel() < cap) {
                 foodData.setSaturation(Math.min(cap, foodData.getSaturationLevel() + saturation));
@@ -53,6 +74,25 @@ public record HungerEffect(
         } else {
             foodData.setSaturation(foodData.getSaturationLevel() + saturation);
         }
+    }
+
+    @Override
+    public List<MutableComponent> getDescription(final Player dragon, final DragonAbilityInstance ability) {
+        MutableComponent description = Component.empty();
+
+        hunger.ifPresent(value -> {
+            float food = value.calculate(ability.level());
+            if (food > 0) {
+                description.append(Component.translatable(ADD_FOOD, food).append("\n"));
+            } else if (food < 0) {
+                description.append(Component.translatable(ADD_EXHAUSTION, food * -4).append("\n"));
+            }
+        });
+
+        conversionRate.ifPresent(value -> description.append(Component.translatable(CONVERSION_RATE, value.calculate(ability.level()))).append("\n"));
+        saturation.ifPresent(value -> description.append(Component.translatable(SATURATION, value.calculate(ability.level()))).append("\n"));
+        maxSaturation.ifPresent(value -> description.append(Component.translatable(MAX_SATURATION, value.calculate(ability.level()))).append("\n"));
+        return List.of(description);
     }
 
     @Override
