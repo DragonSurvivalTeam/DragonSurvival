00c482c04470798f4558e7079b90196a47ae301e
Oblivark <oblivire@users.noreply.github.com>
2025-05-11 14:47:53 -0400

    Use silktouch enchanted pickaxe for silk touch effect, iterate through abilities once, use enum

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/activation/trigger/OnBlockBreak.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/activation/trigger/OnBlockBreak.java
@@ -6,27 +6,37 @@ import by.dragonsurvivalteam.dragonsurvival.common.codecs.Condition;
 import by.dragonsurvivalteam.dragonsurvival.registry.attachments.MagicData;
 import by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.DragonAbilityInstance;
 import by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.activation.PassiveActivation;
+import by.dragonsurvivalteam.dragonsurvival.util.EnchantmentUtils;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.util.StringRepresentable;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.neoforged.neoforge.event.level.BlockEvent;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Optional;
 
-public record OnBlockBreak(Optional<LootItemCondition> condition, Optional<String> cancel_variant) implements ActivationTrigger {
+public record OnBlockBreak(Optional<LootItemCondition> condition, Optional<CancelVariants> cancel_variant) implements ActivationTrigger {
     public static final MapCodec<OnBlockBreak> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
             LootItemCondition.DIRECT_CODEC.optionalFieldOf("condition").forGetter(OnBlockBreak::condition),
-            Codec.STRING.optionalFieldOf("cancel_variant").forGetter(OnBlockBreak::cancel_variant)
+            CancelVariants.CODEC.optionalFieldOf("cancel_variant").forGetter(OnBlockBreak::cancel_variant)
     ).apply(instance, OnBlockBreak::new));
+
+    private static final ItemStack pickaxe = new ItemStack(Items.DIAMOND_PICKAXE, 1);
 
     public static void trigger(final BlockEvent.BreakEvent event) {
         if (event.getPlayer() instanceof ServerPlayer player) {
@@ -40,18 +50,23 @@ public record OnBlockBreak(Optional<LootItemCondition> condition, Optional<Strin
             BlockState blockState = event.getState();
             LootContext context = Condition.blockContext(player, blockPos, blockState);
 
-
             List<DragonAbilityInstance> abilities = MagicData.getData(player).filterPassiveByTrigger(trigger -> trigger.type() == TriggerType.ON_BLOCK_BREAK && trigger.test(context));
-            abilities.forEach(ability -> ability.tick(player));
 
-            boolean silky = abilities.stream().anyMatch(instance -> instance.value().activation() instanceof PassiveActivation passive && passive.trigger() instanceof OnBlockBreak trigger && trigger.getCancelVariant().equals("silk"));
-            boolean indestructible = abilities.stream().anyMatch(instance -> instance.value().activation() instanceof PassiveActivation passive && passive.trigger() instanceof OnBlockBreak trigger && trigger.getCancelVariant().equals("indestructible"));
+            boolean silky = false;
+            boolean indestructible = false;
+
+            for (DragonAbilityInstance ability : abilities) {
+                ability.tick(player);
+                CancelVariants cancelVariant = ((OnBlockBreak) ((PassiveActivation) ability.value().activation()).trigger()).getCancelVariant();
+                silky |= cancelVariant == CancelVariants.SILK;
+                indestructible |= cancelVariant == CancelVariants.INDESTRUCTIBLE;
+            }
 
             if (silky || indestructible) {
                 if (silky) {
-                    ItemStack blockStack = new ItemStack(blockState.getBlock().asItem(), 1);
-                    ItemEntity blockStackEntity = new ItemEntity(level, blockPos.getX(), blockPos.getY(), blockPos.getZ(), blockStack);
-                    level.addFreshEntity(blockStackEntity);
+                    initializePickaxe();
+                    BlockEntity blockEntity =  blockState.hasBlockEntity() ? level.getBlockEntity(blockPos) : null;
+                    Block.dropResources(blockState, level, blockPos, blockEntity, player, pickaxe);
                     level.destroyBlock(blockPos, false);
                 }
                 event.setCanceled(true);
@@ -64,8 +79,15 @@ public record OnBlockBreak(Optional<LootItemCondition> condition, Optional<Strin
         return this.condition.map(condition -> condition.test(context)).orElse(true);
     }
 
-    public String getCancelVariant() {
-        return this.cancel_variant.orElse("");
+    private static void initializePickaxe() {
+        Holder<Enchantment> silk = EnchantmentUtils.getHolder(Enchantments.SILK_TOUCH);
+        if (pickaxe.getEnchantmentLevel(silk) != 1) {
+            pickaxe.enchant(silk, 1);
+        }
+    }
+
+    public CancelVariants getCancelVariant() {
+        return this.cancel_variant.orElse(CancelVariants.NONE);
     }
 
     @Override
@@ -76,5 +98,21 @@ public record OnBlockBreak(Optional<LootItemCondition> condition, Optional<Strin
     @Override
     public MapCodec<? extends ActivationTrigger> codec() {
         return CODEC;
+    }
+
+    public enum CancelVariants implements StringRepresentable {
+        SILK("silk"), INDESTRUCTIBLE("indestructible"), NONE("none");
+
+        public static final Codec<CancelVariants> CODEC = StringRepresentable.fromEnum(CancelVariants::values);
+        private final String name;
+
+        CancelVariants(final String name) {
+            this.name = name;
+        }
+
+        @Override
+        public @NotNull String getSerializedName() {
+            return name;
+        }
     }
 }
