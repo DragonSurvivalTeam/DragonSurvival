300d650a2963b2a4a876f49944ee1c9c76f8dbb4
Cadentem <kai.zahn@yahoo.de>
2025-05-20 18:26:39 +0200

    Update OnBlockBreak.java

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/activation/trigger/OnBlockBreak.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/activation/trigger/OnBlockBreak.java
@@ -6,22 +6,20 @@ import by.dragonsurvivalteam.dragonsurvival.common.codecs.Condition;
 import by.dragonsurvivalteam.dragonsurvival.registry.attachments.MagicData;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
+import cpw.mods.util.Lazy;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.neoforged.neoforge.event.level.BlockEvent;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public record OnBlockBreak(Optional<LootItemCondition> condition) implements ActivationTrigger {
     public static final MapCodec<OnBlockBreak> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
             LootItemCondition.DIRECT_CODEC.optionalFieldOf("condition").forGetter(OnBlockBreak::condition)
     ).apply(instance, OnBlockBreak::new));
-
-
+    
     public static void trigger(final BlockEvent.BreakEvent event) {
         if (event.getPlayer() instanceof ServerPlayer player) {
             DragonStateHandler handler = DragonStateProvider.getData(player);
@@ -29,15 +27,18 @@ public record OnBlockBreak(Optional<LootItemCondition> condition) implements Act
             if (!handler.isDragon()) {
                 return;
             }
-            ServerLevel level = player.serverLevel();
-            BlockPos blockPos = event.getPos();
-            BlockState origBlockState = event.getState();
-            LootContext context = Condition.blockContext(player, blockPos, origBlockState);
 
-            MagicData.getData(player).filterPassiveByTrigger(trigger -> trigger.type() == TriggerType.ON_BLOCK_BREAK && trigger.test(context)).forEach(ability -> ability.tick(player));
+            AtomicBoolean triggeredAbility = new AtomicBoolean(false);
+            Lazy<LootContext> context = Lazy.of(Condition.blockContext(player, event.getPos(), event.getState()));
 
-            /** Cancel because the block changed after the abilities ticked */
-            if (level.getBlockState(blockPos) != origBlockState) {
+            MagicData.getData(player).filterPassiveByTrigger(trigger -> trigger.type() == TriggerType.ON_BLOCK_BREAK && trigger.test(context.get()))
+                    .forEach(ability -> {
+                        ability.tick(player);
+                        triggeredAbility.set(true);
+                    });
+
+            if (triggeredAbility.get() && player.level().getBlockState(event.getPos()) != event.getState()) {
+                // Cancel because the block changed after the abilities ticked
                 event.setCanceled(true);
             }
         }
