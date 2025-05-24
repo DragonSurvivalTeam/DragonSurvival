47ea85c8f1f3e61921cfc33d68b75adac3c8aaf4
Oblivark <oblivire@users.noreply.github.com>
2025-05-23 21:34:58 -0400

    Drop EntityPredicate, add action_type to allow setting percentage

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/entity_effects/DragonGrowthEffect.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/entity_effects/DragonGrowthEffect.java
@@ -2,39 +2,26 @@ package by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.entity_effe
 
 import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateHandler;
 import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateProvider;
-import by.dragonsurvivalteam.dragonsurvival.common.codecs.BlockVision;
-import by.dragonsurvivalteam.dragonsurvival.registry.DSAttributes;
-import by.dragonsurvivalteam.dragonsurvival.registry.datagen.Translation;
-import by.dragonsurvivalteam.dragonsurvival.registry.datagen.lang.LangKey;
+import by.dragonsurvivalteam.dragonsurvival.common.codecs.MiscCodecs;
 import by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.DragonAbilityInstance;
-import by.dragonsurvivalteam.dragonsurvival.util.DSColors;
+import by.dragonsurvivalteam.dragonsurvival.registry.dragon.stage.DragonStage;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.advancements.critereon.EntityPredicate;
 import net.minecraft.core.Holder;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.StringRepresentable;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.enchantment.LevelBasedValue;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-import java.util.Optional;
-
-public record DragonGrowthEffect(DragonGrowthEffect.GrowthType growth_type, LevelBasedValue amount, LevelBasedValue probability, Optional<EntityPredicate> validEntities) implements AbilityEntityEffect {
+public record DragonGrowthEffect(DragonGrowthEffect.GrowthType growth_type, DragonGrowthEffect.ActionType action_type, LevelBasedValue amount, LevelBasedValue probability) implements AbilityEntityEffect {
     public static final MapCodec<DragonGrowthEffect> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
             GrowthType.CODEC.fieldOf("growth_type").forGetter(DragonGrowthEffect::growth_type),
+            ActionType.CODEC.fieldOf("action_type").forGetter(DragonGrowthEffect::action_type),
             LevelBasedValue.CODEC.fieldOf("amount").forGetter(DragonGrowthEffect::amount),
-            LevelBasedValue.CODEC.fieldOf("probability").forGetter(DragonGrowthEffect::probability),
-            EntityPredicate.CODEC.optionalFieldOf("valid_entities").forGetter(DragonGrowthEffect::validEntities)
+            LevelBasedValue.CODEC.fieldOf("probability").forGetter(DragonGrowthEffect::probability)
     ).apply(instance, DragonGrowthEffect::new));
 
     @Override
@@ -50,31 +37,48 @@ public record DragonGrowthEffect(DragonGrowthEffect.GrowthType growth_type, Leve
                 return;
             }
 
-            if (probability.calculate(ability.level()) < dragon.getRandom().nextDouble()) {
-                return;
+            double amount = amount().calculate(ability.level());
+            if (action_type == ActionType.PERCENT) {
+                Holder<DragonStage> dragonStage = handler.stage();
+                MiscCodecs.Bounds growthRange = dragonStage.value().growthRange();
+                double maxSize = growthRange.max();
+                double minSize = growthRange.min();
+                // Maybe get maximum overall size instead of the current stage?
+                amount = amount * 0.01 * maxSize + amount * 0.01 * minSize;
             }
 
-            double amount = amount().calculate(ability.level());
-            double targetGrowth = switch (growth_type) {
-                case GrowthType.ADD -> handler.getGrowth() + amount;
-                case GrowthType.SUBTRACT -> handler.getGrowth() - amount;
-                default -> amount;
+            double targetGrowth = amount;
+            if (growth_type == GrowthType.ADD) {
+                targetGrowth += handler.getGrowth();
             };
 
-            ServerLevel level = dragon.serverLevel();
-            if (validEntities.isEmpty() || validEntities.get().matches(level, dragon.position(), target)) {
-                handler.setDesiredGrowth(targetPlayer, targetGrowth);
-            }
+            handler.setDesiredGrowth(targetPlayer, targetGrowth);
         }
     }
 
     public enum GrowthType implements StringRepresentable {
-        SET("set"), ADD("add"), SUBTRACT("subtract");
+        SET("set"), ADD("add");
 
         public static final Codec<DragonGrowthEffect.GrowthType> CODEC = StringRepresentable.fromEnum(DragonGrowthEffect.GrowthType::values);
         private final String name;
 
         GrowthType(final String name) {
+            this.name = name;
+        }
+
+        @Override
+        public @NotNull String getSerializedName() {
+            return name;
+        }
+    }
+
+    public enum ActionType implements StringRepresentable {
+        PERCENT("percent"), FLAT("flat");
+
+        public static final Codec<DragonGrowthEffect.ActionType> CODEC = StringRepresentable.fromEnum(DragonGrowthEffect.ActionType::values);
+        private final String name;
+
+        ActionType(final String name) {
             this.name = name;
         }
 
