6fe4ffee80803dc7d94dc400c3f79f74f6d33d58
Oblivark <oblivire@users.noreply.github.com>
2025-05-28 01:00:46 -0400

    Update to use EntityEquipmentPredicate (but poorly)

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/entity_effects/DropItemEffect.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/dragon/ability/entity_effects/DropItemEffect.java
@@ -3,8 +3,7 @@ package by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.entity_effe
 import by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.DragonAbilityInstance;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.advancements.critereon.EntityPredicate;
-import net.minecraft.advancements.critereon.ItemPredicate;
+import net.minecraft.advancements.critereon.EntityEquipmentPredicate;
 import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
@@ -12,21 +11,23 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.LevelBasedValue;
 import net.minecraft.world.phys.Vec3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-public record DropItemEffect(Optional<ItemPredicate> itemPredicate, LevelBasedValue probability, Optional<Holder<SoundEvent>> sound, Optional<EntityPredicate> validEntities) implements AbilityEntityEffect {
+public record DropItemEffect(EntityEquipmentPredicate equipmentPredicate, LevelBasedValue probability, Optional<Holder<SoundEvent>> sound) implements AbilityEntityEffect {
 
     public static final MapCodec<DropItemEffect> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-            ItemPredicate.CODEC.optionalFieldOf("item_predicate").forGetter(DropItemEffect::itemPredicate),
+            EntityEquipmentPredicate.CODEC.fieldOf("equipment_predicate").forGetter(DropItemEffect::equipmentPredicate),
             LevelBasedValue.CODEC.fieldOf("probability").forGetter(DropItemEffect::probability),
-            SoundEvent.CODEC.optionalFieldOf("sound").forGetter(DropItemEffect::sound),
-            EntityPredicate.CODEC.optionalFieldOf("valid_entities").forGetter(DropItemEffect::validEntities)
+            SoundEvent.CODEC.optionalFieldOf("sound").forGetter(DropItemEffect::sound)
     ).apply(instance, DropItemEffect::new));
 
     @Override
@@ -36,32 +37,50 @@ public record DropItemEffect(Optional<ItemPredicate> itemPredicate, LevelBasedVa
                 return;
             }
 
-            if (probability.calculate(ability.level()) < dragon.getRandom().nextDouble()) {
-                return;
+            // EntityEquipmentPredicate is a sub-predicate of EntityPredicate - maybe use that?  Or SlotsPredicate
+            // The below should be simplified, but I can't think of how at the moment and feedback would be helpful
+            List<ItemStack> itemsToDrop = new ArrayList<>();
+            if (equipmentPredicate.head().isPresent() && equipmentPredicate.head().get().test(livingEntity.getItemBySlot(EquipmentSlot.HEAD))) {
+                itemsToDrop.add(livingEntity.getItemBySlot(EquipmentSlot.HEAD));
+                livingEntity.setItemSlot(EquipmentSlot.HEAD, ItemStack.EMPTY);
             }
-
-            ServerLevel level = dragon.serverLevel();
-            if (validEntities.isEmpty() || validEntities.get().matches(level, dragon.position(), target)) {
-
-                /** Add a CODEC here to allow for a list of things to drop instead? */
-                ItemStack stack = livingEntity.getMainHandItem();
-
-                if (itemPredicate.map(predicate -> !predicate.test(stack)).orElse(false)) {
-                    return;
-                }
-
-                Vec3 pos = livingEntity.position();
-
-                ItemStack droppedStack = new ItemStack(stack.getItem(), 1);
-                ItemEntity droppedStackEntity = new ItemEntity(level, pos.x(), pos.y(), pos.z(), droppedStack);
-
-                level.addFreshEntity(droppedStackEntity);
+            if (equipmentPredicate.chest().isPresent() && equipmentPredicate.chest().get().test(livingEntity.getItemBySlot(EquipmentSlot.CHEST))) {
+                itemsToDrop.add(livingEntity.getItemBySlot(EquipmentSlot.CHEST));
+                livingEntity.setItemSlot(EquipmentSlot.CHEST, ItemStack.EMPTY);
+            }
+            if (equipmentPredicate.legs().isPresent() && equipmentPredicate.legs().get().test(livingEntity.getItemBySlot(EquipmentSlot.LEGS))) {
+                itemsToDrop.add(livingEntity.getItemBySlot(EquipmentSlot.LEGS));
+                livingEntity.setItemSlot(EquipmentSlot.LEGS, ItemStack.EMPTY);
+            }
+            if (equipmentPredicate.feet().isPresent() && equipmentPredicate.feet().get().test(livingEntity.getItemBySlot(EquipmentSlot.FEET))) {
+                itemsToDrop.add(livingEntity.getItemBySlot(EquipmentSlot.FEET));
+                livingEntity.setItemSlot(EquipmentSlot.FEET, ItemStack.EMPTY);
+            }
+            if (equipmentPredicate.body().isPresent() && equipmentPredicate.body().get().test(livingEntity.getItemBySlot(EquipmentSlot.BODY))) {
+                itemsToDrop.add(livingEntity.getItemBySlot(EquipmentSlot.BODY));
+                livingEntity.setItemSlot(EquipmentSlot.BODY, ItemStack.EMPTY);
+            }
+            if (equipmentPredicate.mainhand().isPresent() && equipmentPredicate.mainhand().get().test(livingEntity.getItemBySlot(EquipmentSlot.MAINHAND))) {
+                itemsToDrop.add(livingEntity.getItemBySlot(EquipmentSlot.MAINHAND));
                 livingEntity.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
-                /** Below is possibly redundant */
+                // Below is possibly redundant
                 livingEntity.stopUsingItem();
-
-                sound.ifPresent(soundHolder -> dragon.level().playSound(null, target, soundHolder.value(), SoundSource.BLOCKS, 1, 1));
             }
+            if (equipmentPredicate.offhand().isPresent() && equipmentPredicate.offhand().get().test(livingEntity.getItemBySlot(EquipmentSlot.OFFHAND))) {
+                itemsToDrop.add(livingEntity.getItemBySlot(EquipmentSlot.OFFHAND));
+                livingEntity.setItemInHand(InteractionHand.OFF_HAND, ItemStack.EMPTY);
+            }
+
+            Vec3 pos = livingEntity.position();
+            ServerLevel level = dragon.serverLevel();
+
+            for (ItemStack item : itemsToDrop) {
+                // Unsure if this will keep the item durability & other data intact
+                ItemEntity droppedStackEntity = new ItemEntity(level, pos.x(), pos.y(), pos.z(), item);
+                level.addFreshEntity(droppedStackEntity);
+            }
+
+            sound.ifPresent(soundHolder -> dragon.level().playSound(null, target, soundHolder.value(), SoundSource.BLOCKS, 1, 1));
         }
 
     }
