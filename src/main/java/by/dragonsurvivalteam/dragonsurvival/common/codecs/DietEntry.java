863fe7de8602f973dbf90adc679a95c57d7e6c92
Kai <kai.zahn@yahoo.de>
2025-11-28 12:22:19 +0100

    add flag to retain original food effects for diet entries - default is true - previously they were *not* copied over + add proper builder for diet entries

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/codecs/DietEntry.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/codecs/DietEntry.java
@@ -1,60 +1,30 @@
 package by.dragonsurvivalteam.dragonsurvival.common.codecs;
 
+import by.dragonsurvivalteam.dragonsurvival.DragonSurvival;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Supplier;
 
-// TODO :: add ability to retain the original effects of the item (through a flag?)
-public record DietEntry(String items, Optional<FoodProperties> properties) {
-    // Copied from FoodProperties.java. Trying to AT this didn't work out well.
-    public static final float DEFAULT_EAT_SECONDS = 1.6f;
-
+public record DietEntry(String items, Optional<FoodProperties> properties, boolean retainEffects) {
     public static final Codec<DietEntry> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             ResourceLocationWrapper.validatedCodec().fieldOf("items").forGetter(DietEntry::items),
-            FoodProperties.DIRECT_CODEC.optionalFieldOf("properties").forGetter(DietEntry::properties)
+            FoodProperties.DIRECT_CODEC.optionalFieldOf("properties").forGetter(DietEntry::properties),
+            Codec.BOOL.optionalFieldOf("retain_effects", true).forGetter(DietEntry::retainEffects)
     ).apply(instance, DietEntry::new));
-
-    public static DietEntry from(final ResourceLocation location) {
-        return from(location.toString());
-    }
-
-    public static DietEntry from(final String location) {
-        return from(location, null);
-    }
-
-    public static DietEntry from(final TagKey<Item> tag) {
-        return from(tag, null);
-    }
-
-    public static DietEntry from(final TagKey<Item> tag, final FoodProperties properties) {
-        return from("#" + tag.location(), properties);
-    }
-
-    public static DietEntry from(final Item item) {
-        return from(item, null);
-    }
-
-    public static DietEntry from(final Item item, final FoodProperties properties) {
-        //noinspection deprecation,DataFlowIssue -> ignore deprecated / key is present
-        return from(item.builtInRegistryHolder().getKey().location(), properties);
-    }
-
-    public static DietEntry from(final ResourceLocation location, final FoodProperties properties) {
-        return from(location.toString(), properties);
-    }
-
-    public static DietEntry from(final String location, final FoodProperties properties) {
-        return new DietEntry(location, Optional.ofNullable(properties));
-    }
 
     public static Map<Item, FoodProperties> map(final List<DietEntry> entries) {
         Map<Item, FoodProperties> diet = new HashMap<>();
@@ -64,16 +34,46 @@ public record DietEntry(String items, Optional<FoodProperties> properties) {
                 Item item = BuiltInRegistries.ITEM.get(resource);
 
                 if (item != null) {
-                    FoodProperties properties = entry.properties.orElse(item.getDefaultInstance().getFoodProperties(null));
+                    FoodProperties properties = entry.properties().orElse(item.getDefaultInstance().getFoodProperties(null));
 
-                    if (properties != null) {
-                        diet.put(item, properties);
+                    if (properties == null) {
+                        DragonSurvival.LOGGER.warn("Diet entry [{}] has neither original nor custom food properties - item will not be edible", entry);
+                        return;
                     }
+
+                    List<FoodProperties.PossibleEffect> effects = new ArrayList<>(properties.effects());
+
+                    if (entry.retainEffects() && entry.properties().isPresent()) {
+                        // Custom food properties were specified -> retain the effects of the original properties
+                        FoodProperties original = item.getDefaultInstance().getFoodProperties(null);
+
+                        if (original != null) {
+                            effects.addAll(original.effects());
+                        }
+                    } else if (!entry.retainEffects() && entry.properties().isEmpty()) {
+                        // Remove the original effects
+                        effects.clear();
+                    }
+
+                    properties = new FoodProperties(
+                            properties.nutrition(),
+                            properties.saturation(),
+                            properties.canAlwaysEat(),
+                            properties.eatSeconds(),
+                            properties.usingConvertsTo(),
+                            effects
+                    );
+
+                    diet.put(item, properties);
                 }
             });
         }
 
         return diet;
+    }
+
+    public static Builder create() {
+        return new Builder();
     }
 
     @Override
@@ -92,5 +92,103 @@ public record DietEntry(String items, Optional<FoodProperties> properties) {
     @Override
     public int hashCode() {
         return items.hashCode();
+    }
+
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    public static class Builder {
+        // Copied from FoodProperties.java. Trying to AT this didn't work out well.
+        private static final float DEFAULT_EAT_SECONDS = 1.6f;
+
+        private String items;
+        private Optional<FoodProperties> properties = Optional.empty();
+        private boolean retainEffects = true;
+
+        private boolean customProperties;
+        
+        private int nutriton;
+        private float saturation;
+        private boolean canAlwaysEat;
+        private float seconds = DEFAULT_EAT_SECONDS;
+
+        private final List<FoodProperties.PossibleEffect> effects = new ArrayList<>();
+        private Optional<ItemStack> convertsTo = Optional.empty();
+
+        public Builder items(final String items) {
+            this.items = items;
+            return this;
+        }
+
+        public Builder items(final TagKey<Item> tag) {
+            return items("#" + tag.location());
+        }
+
+        public Builder items(final ResourceLocation location) {
+            return items(location.toString());
+        }
+
+        public Builder items(final Item item) {
+            //noinspection deprecation, DataFlowIssue -> ignore deprecated / key is present
+            return items(item.builtInRegistryHolder().getKey().location());
+        }
+
+        public Builder nutrition(final int nutrition) {
+            this.nutriton = nutrition;
+            this.customProperties = true;
+            return this;
+        }
+
+        public Builder saturation(final float saturation) {
+            this.saturation = saturation;
+            this.customProperties = true;
+            return this;
+        }
+
+        public Builder canAlwaysEat() {
+            this.canAlwaysEat = true;
+            this.customProperties = true;
+            return this;
+        }
+
+        public Builder seconds(final float seconds) {
+            this.seconds = seconds;
+            this.customProperties = true;
+            return this;
+        }
+
+        public Builder fast() {
+            this.seconds = DEFAULT_EAT_SECONDS / 2;
+            this.customProperties = true;
+            return this;
+        }
+
+        public Builder effect(final Supplier<MobEffectInstance> effect, final float probability) {
+            this.effects.add(new FoodProperties.PossibleEffect(effect, probability));
+            this.customProperties = true;
+            return this;
+        }
+
+        public Builder convertsTo(final ItemLike item) {
+            this.convertsTo = Optional.of(item.asItem().getDefaultInstance());
+            this.customProperties = true;
+            return this;
+        }
+
+        public Builder properties(final FoodProperties properties) {
+            this.properties = Optional.ofNullable(properties);
+            return this;
+        }
+
+        public Builder removeOriginalEffects() {
+            this.retainEffects = false;
+            return this;
+        }
+
+        public DietEntry build() {
+            if (customProperties && properties.isEmpty()) {
+                properties = Optional.of(new FoodProperties(nutriton, saturation, canAlwaysEat, seconds, convertsTo, effects));
+            }
+
+            return new DietEntry(items, properties, retainEffects);
+        }
     }
 }
