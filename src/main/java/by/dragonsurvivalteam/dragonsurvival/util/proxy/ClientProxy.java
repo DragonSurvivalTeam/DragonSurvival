1186d2e9a8d94bcf8a96938b469c7aa94c5441c5
Seelder <zlschect@gmail.com>
2026-01-02 14:39:37 -0500

    Fix ClientProxy

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/util/proxy/ClientProxy.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/util/proxy/ClientProxy.java
@@ -32,9 +32,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.client.network.ClientPacketDistributor;
-import net.neoforged.neoforge.network.PacketDistributor;
 import org.jetbrains.annotations.Nullable;
-import software.bernie.geckolib.cache.GeckoLibCache;
 import software.bernie.geckolib.cache.GeckoLibResources;
 import software.bernie.geckolib.loading.object.BakedAnimations;
 
