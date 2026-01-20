6dc9e90af1efa33fbdce8d5fefb68c078a902435
seelderr <apersonwhoexistsinemail@gmail.com>
2026-01-19 16:23:24 -0800

    Conditions

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/conditions/ItemCondition.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/conditions/ItemCondition.java
@@ -1,10 +1,12 @@
 package by.dragonsurvivalteam.dragonsurvival.common.conditions;
 
-import net.minecraft.advancements.criterion.ItemPotionsPredicate;
+import net.minecraft.advancements.criterion.DataComponentMatchers;
 import net.minecraft.advancements.criterion.ItemPredicate;
-import net.minecraft.advancements.criterion.ItemSubPredicates;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderSet;
+import net.minecraft.core.component.predicates.DataComponentPredicates;
+import net.minecraft.core.component.predicates.PotionsPredicate;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.alchemy.Potion;
@@ -12,15 +14,19 @@ import net.minecraft.world.level.ItemLike;
 
 public class ItemCondition {
     public static ItemPredicate is(final ItemLike... items) {
-        return ItemPredicate.Builder.item().of(items).build();
+        return ItemPredicate.Builder.item().of(BuiltInRegistries.ITEM, items).build();
     }
 
     public static ItemPredicate is(final TagKey<Item> tag) {
-        return ItemPredicate.Builder.item().of(tag).build();
+        return ItemPredicate.Builder.item().of(BuiltInRegistries.ITEM, tag).build();
     }
 
     @SafeVarargs
     public static ItemPredicate hasPotion(final Holder<Potion>... potions) {
-        return ItemPredicate.Builder.item().withSubPredicate(ItemSubPredicates.POTIONS, new ItemPotionsPredicate(HolderSet.direct(potions))).build();
+        return ItemPredicate.Builder.item().withComponents(
+                DataComponentMatchers.Builder.components()
+                        .partial(DataComponentPredicates.POTIONS, new PotionsPredicate(HolderSet.direct(potions)))
+                        .build()
+        ).build();
     }
 }
