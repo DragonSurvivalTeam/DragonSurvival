61165c07654010c8accd6be4c026bd3b3c095ac6
seelderr <apersonwhoexistsinemail@gmail.com>
2026-04-09 13:50:42 -0700

    Fixed helmet rendering

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/client/render/blocks/HelmetEntityRenderer.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/client/render/blocks/HelmetEntityRenderer.java
@@ -1,60 +1,72 @@
 package by.dragonsurvivalteam.dragonsurvival.client.render.blocks;
 
+import by.dragonsurvivalteam.dragonsurvival.DragonSurvival;
+import by.dragonsurvivalteam.dragonsurvival.common.blocks.HelmetBlock;
+import by.dragonsurvivalteam.dragonsurvival.registry.DSBlocks;
 import by.dragonsurvivalteam.dragonsurvival.server.tileentity.HelmetBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.model.object.skull.SkullModel;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.blockentity.SkullBlockRenderer;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
+import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
+import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.client.renderer.state.level.CameraRenderState;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.phys.Vec3;
 
+import java.util.Map;
 
+public class HelmetEntityRenderer<T extends HelmetBlockEntity> implements BlockEntityRenderer<T, HelmetEntityRenderer.State> {
+    private static final Map<HelmetBlock, Identifier> TEXTURE_BY_TYPE = Map.of(
+        DSBlocks.GRAY_KNIGHT_HELMET.value(), DragonSurvival.res("textures/block/gray_knight_helmet.png"),
+        DSBlocks.GOLDEN_KNIGHT_HELMET.value(), DragonSurvival.res("textures/block/golden_knight_helmet.png"),
+        DSBlocks.BLACK_KNIGHT_HELMET.value(), DragonSurvival.res("textures/block/black_knight_helmet.png")
+    );
 
-public class HelmetEntityRenderer<T extends HelmetBlockEntity> implements BlockEntityRenderer<T, BlockEntityRenderState> {
-//    private static final Map<Block, Identifier> TEXTURE_BY_TYPE = Util.make(Maps.newHashMap(), IdentifierHashMap -> {
-//        IdentifierHashMap.put(DSBlocks.GRAY_KNIGHT_HELMET.get(), Identifier.fromNamespaceAndPath(MODID, "textures/block/gray_knight_helmet.png"));
-//        IdentifierHashMap.put(DSBlocks.GOLDEN_KNIGHT_HELMET.get(), Identifier.fromNamespaceAndPath(MODID, "textures/block/golden_knight_helmet.png"));
-//        IdentifierHashMap.put(DSBlocks.BLACK_KNIGHT_HELMET.get(), Identifier.fromNamespaceAndPath(MODID, "textures/block/black_knight_helmet.png"));
-//    });
-//    static SkullModel humanoidHeadModel = new SkullModel(Minecraft.getInstance().getEntityModels().bakeLayer(ModelLayers.PLAYER_HEAD));
+    private final SkullModel humanoidHeadModel;
 
-    public HelmetEntityRenderer(BlockEntityRendererProvider.Context context) { /* Nothing to do */ }
-
-//    @Override
-//    public void render(HelmetBlockEntity helmetEntity, float partialTick, @NotNull PoseStack PoseStack, @NotNull MultiBufferSource renderTypeBuffer, int packedLight, int packedOverlay) {
-//        BlockState blockstate = helmetEntity.getBlockState();
-//        float f1 = 22.5F * blockstate.getValue(SkullBlock.ROTATION);
-//        renderHelmet(null, f1, blockstate.getBlock(), 0, PoseStack, renderTypeBuffer, packedLight);
-//    }
-//
-//    public static void renderHelmet(
-//            @Nullable Direction direction, float p_228879_1_, Block helmetBlock, float p_228879_4_, PoseStack PoseStack, MultiBufferSource renderTypeBuffer, int p_228879_7_) {
-//        PoseStack.pushMatrix();
-//        if (direction == null) {
-//            PoseStack.translate(0.5D, 0.0D, 0.5D);
-//        } else {
-//            PoseStack.translate(0.5F - (float) direction.getStepX() * 0.25F, 0.25D, 0.5F - (float) direction.getStepZ() * 0.25F);
-//        }
-//
-//        PoseStack.scale(-1.0F, -1.0F, 1.0F);
-//        VertexConsumer ivertexbuilder = renderTypeBuffer.getBuffer(getRenderType(helmetBlock));
-//        humanoidHeadModel.setupAnim(p_228879_4_, p_228879_1_, 0.0F);
-//        humanoidHeadModel.renderToBuffer(PoseStack, ivertexbuilder, p_228879_7_, OverlayTexture.NO_OVERLAY);
-//        PoseStack.popMatrix();
-//    }
-//
-//    private static RenderType getRenderType(Block block) {
-//        Identifier Identifier = TEXTURE_BY_TYPE.get(block);
-//        return RenderType.entityCutoutNoCullZOffset(Identifier);
-//    }
-
-    @Override
-    public BlockEntityRenderState createRenderState() {
-        return null;
+    public HelmetEntityRenderer(final BlockEntityRendererProvider.Context context) {
+        humanoidHeadModel = new SkullModel(context.bakeLayer(ModelLayers.PLAYER_HEAD));
     }
 
     @Override
-    public void submit(BlockEntityRenderState renderState, PoseStack poseStack, SubmitNodeCollector nodeCollector, CameraRenderState cameraRenderState) {
-        // FIXME
+    public State createRenderState() {
+        return new State();
+    }
+
+    @Override
+    public void extractRenderState(
+        final T blockEntity,
+        final State state,
+        final float partialTicks,
+        final Vec3 cameraPosition,
+        final ModelFeatureRenderer.CrumblingOverlay breakProgress
+    ) {
+        BlockEntityRenderer.super.extractRenderState(blockEntity, state, partialTicks, cameraPosition, breakProgress);
+        state.yRot = -22.5F * blockEntity.getBlockState().getValue(HelmetBlock.ROTATION);
+        state.renderType = RenderTypes.entityCutoutZOffset(
+            TEXTURE_BY_TYPE.getOrDefault(blockEntity.getBlockState().getBlock(), DragonSurvival.MISSING_TEXTURE)
+        );
+    }
+
+    @Override
+    public void submit(final State state, final PoseStack poseStack, final SubmitNodeCollector nodeCollector, final CameraRenderState cameraRenderState) {
+        poseStack.pushPose();
+        poseStack.translate(0.5F, 0.0F, 0.5F);
+        poseStack.mulPose(Axis.YP.rotationDegrees(state.yRot));
+        poseStack.scale(-1.0F, -1.0F, 1.0F);
+        SkullBlockRenderer.submitSkull(0.0F, poseStack, nodeCollector, state.lightCoords, humanoidHeadModel, state.renderType, 0, state.breakProgress);
+        poseStack.popPose();
+    }
+
+    public static class State extends BlockEntityRenderState {
+        public float yRot;
+        public RenderType renderType = RenderTypes.entityCutoutZOffset(DragonSurvival.MISSING_TEXTURE);
     }
 }
