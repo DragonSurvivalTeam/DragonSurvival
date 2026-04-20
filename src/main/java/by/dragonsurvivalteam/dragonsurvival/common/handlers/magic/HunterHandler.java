0edaa62b0722dc371a3ef0282a14464a63d75134
Cadentem <kai.zahn@yahoo.de>
2026-04-20 10:33:51 +0200

    skip message with iris

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/handlers/magic/HunterHandler.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/handlers/magic/HunterHandler.java
@@ -1,6 +1,7 @@
 package by.dragonsurvivalteam.dragonsurvival.common.handlers.magic;
 
 import by.dragonsurvivalteam.dragonsurvival.DragonSurvival;
+import by.dragonsurvivalteam.dragonsurvival.compat.ModCheck;
 import by.dragonsurvivalteam.dragonsurvival.config.obj.ConfigOption;
 import by.dragonsurvivalteam.dragonsurvival.config.obj.ConfigSide;
 import by.dragonsurvivalteam.dragonsurvival.network.magic.SyncHunterStacksRemoval;
@@ -126,7 +127,7 @@ public class HunterHandler { // FIXME :: disable shadows in EntityRenderDispatch
     }
 
     public static void informUser(final LivingEntity entity) {
-        if (!SEND_INFO_MESSAGE) {
+        if (!SEND_INFO_MESSAGE || ModCheck.isModLoaded(ModCheck.IRIS)) {
             return;
         }
 
