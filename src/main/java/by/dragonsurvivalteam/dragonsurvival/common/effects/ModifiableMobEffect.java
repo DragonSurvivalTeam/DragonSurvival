01c6b0b45456ca9defd25d5ff4e7d080174fbbd5
seelderr <apersonwhoexistsinemail@gmail.com>
2026-07-25 00:17:14 -0700

    Backport incurable mob effects

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/effects/ModifiableMobEffect.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/effects/ModifiableMobEffect.java
@@ -3,11 +3,11 @@ package by.dragonsurvivalteam.dragonsurvival.common.effects;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraftforge.common.EffectCure;
-import org.jetbrains.annotations.NotNull;
+import net.minecraftforge.event.entity.living.MobEffectEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-import java.util.Set;
-
+@EventBusSubscriber
 public class ModifiableMobEffect extends MobEffect {
     private final boolean incurable;
 
@@ -16,12 +16,15 @@ public class ModifiableMobEffect extends MobEffect {
         this.incurable = incurable;
     }
 
-    @Override
-    public void fillEffectCures(@NotNull final Set<EffectCure> cures, @NotNull final MobEffectInstance effectInstance) {
-        if (incurable) {
-            cures.clear();
-        } else {
-            super.fillEffectCures(cures, effectInstance);
+    public boolean isIncurable() {
+        return incurable;
+    }
+
+    @SubscribeEvent
+    public static void clearCurativeItems(final MobEffectEvent.Added event) {
+        MobEffectInstance instance = event.getEffectInstance();
+        if (instance.getEffect() instanceof ModifiableMobEffect effect && effect.isIncurable()) {
+            instance.setCurativeItems(java.util.List.of());
         }
     }
 }
--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/effects/TradeEffect.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/effects/TradeEffect.java
@@ -1,21 +1,10 @@
 package by.dragonsurvivalteam.dragonsurvival.common.effects;
 
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraftforge.common.EffectCure;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.Set;
-
-public class TradeEffect extends MobEffect {
+public class TradeEffect extends ModifiableMobEffect {
     public TradeEffect(final MobEffectCategory type, int color) {
-        super(type, color);
-    }
-
-    @Override // Makes the effect incurable
-    public void fillEffectCures(final Set<EffectCure> cures, @NotNull final MobEffectInstance effectInstance) {
-        cures.clear();
+        super(type, color, true);
     }
 
     @Override
