965940e620ca29d0f96ce99d8087f744dc19f161
seelderr <apersonwhoexistsinemail@gmail.com>
2026-07-25 11:01:14 -0700

    Backport custom glow particle base

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/particles/CustomGlowParticle.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/particles/CustomGlowParticle.java
@@ -11,29 +11,55 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.GlowParticle;
 import net.minecraft.client.particle.Particle;
 import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.ParticleRenderType;
 import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.particle.TextureSheetParticle;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.util.FastColor;
+import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 /** Basically just a copy of {@link GlowParticle.GlowSquidProvider} but allows setting a custom color */
-public class CustomGlowParticle extends GlowParticle {
+public class CustomGlowParticle extends TextureSheetParticle {
     /** Allows each particle to start at a separate section of the (potential) color range */
     private final double colorOffset;
+    private final SpriteSet sprites;
     private List<Integer> colors;
 
     protected CustomGlowParticle(final ClientLevel level, final double x, final double y, final double z, final double blockId, final double ySpeed, final double colorOffset, final SpriteSet sprites) {
-        super(level, x, y, z, 0.5 - level.getRandom().nextDouble(), ySpeed, 0.5 - level.getRandom().nextDouble(), sprites);
+        super(level, x, y, z, 0.5 - level.getRandom().nextDouble(), ySpeed, 0.5 - level.getRandom().nextDouble());
+        friction = 0.96F;
+        speedUpWhenYMotionIsBlocked = true;
+        this.sprites = sprites;
+        quadSize *= 0.75F;
+        hasPhysics = false;
+        setSpriteFromAge(sprites);
         this.colorOffset = colorOffset;
+    }
+
+    @Override
+    public ParticleRenderType getRenderType() {
+        return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
+    }
+
+    @Override
+    public int getLightColor(final float partialTick) {
+        float progress = Mth.clamp(((float) age + partialTick) / (float) lifetime, 0.0F, 1.0F);
+        int light = super.getLightColor(partialTick);
+        int blockLight = light & 255;
+        int skyLight = light >> 16 & 255;
+        blockLight = Math.min(blockLight + (int) (progress * 15.0F * 16.0F), 240);
+        return blockLight | skyLight << 16;
     }
 
     @Override
     public void tick() {
         super.tick();
+        setSpriteFromAge(sprites);
 
         if (colors != null) {
             int argb = DSColors.withAlpha(Functions.lerpColor(colors, 1, colorOffset), 1);
