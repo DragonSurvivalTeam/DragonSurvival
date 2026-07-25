b6b6f499e6fe821825b2baedc3aa99768cbdd3c8
seelderr <apersonwhoexistsinemail@gmail.com>
2026-07-25 08:39:11 -0700

    Omit unavailable optional mod mixins

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/compat/create/SkyhookRendererHelper.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/compat/create/SkyhookRendererHelper.java
@@ -1,18 +1,9 @@
 package by.dragonsurvivalteam.dragonsurvival.compat.create;
 
-import by.dragonsurvivalteam.dragonsurvival.compat.ModID;
-import by.dragonsurvivalteam.dragonsurvival.mixins.create.PlayerSkyhookRendererAccessor;
-
-import java.util.Set;
 import java.util.UUID;
 
 public class SkyhookRendererHelper {
     public static boolean isPlayerRidingSkyhook(UUID uuid) {
-        if (ModID.CREATE.isLoaded()) {
-            Set<UUID> hangingPlayers = PlayerSkyhookRendererAccessor.dragonSurvival$getHangingPlayers();
-            return hangingPlayers != null && hangingPlayers.contains(uuid);
-        } else {
-            return false;
-        }
+        return false;
     }
 }
