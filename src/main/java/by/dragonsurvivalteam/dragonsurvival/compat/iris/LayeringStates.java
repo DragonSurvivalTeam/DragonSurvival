691665541c8baf3f3da8db444f74b8127a8a4c33
seelderr <apersonwhoexistsinemail@gmail.com>
2026-07-25 10:58:37 -0700

    Backport Iris model view layering

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/compat/iris/LayeringStates.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/compat/iris/LayeringStates.java
@@ -1,19 +1,21 @@
 package by.dragonsurvivalteam.dragonsurvival.compat.iris;
 
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.RenderStateShard;
-import org.joml.Matrix4fStack;
 
 // TODO :: This is from 1.21.4. When we update, we can remove this.
 public class LayeringStates {
     public static final RenderStateShard.LayeringStateShard VIEW_OFFSET_Z_LAYERING_FORWARD = new RenderStateShard.LayeringStateShard(
             "view_offset_z_layering_forward", () -> {
-        Matrix4fStack matrix4fStack = RenderSystem.getModelViewStack();
-        matrix4fStack.pushMatrix();
-        matrix4fStack.scale(2.0F / 4096.0F);
+        PoseStack poseStack = RenderSystem.getModelViewStack();
+        poseStack.pushPose();
+        poseStack.scale(2.0F / 4096.0F, 2.0F / 4096.0F, 2.0F / 4096.0F);
+        RenderSystem.applyModelViewMatrix();
     }, () -> {
-        Matrix4fStack matrix4fStack = RenderSystem.getModelViewStack();
-        matrix4fStack.popMatrix();
+        PoseStack poseStack = RenderSystem.getModelViewStack();
+        poseStack.popPose();
+        RenderSystem.applyModelViewMatrix();
     }
     );
 }
