127bfab421fd1ea8c14f9cb589cabf1f44556d64
Cadentem <kai.zahn@yahoo.de>
2026-07-28 07:54:52 +0200

    adjust anchor fuel text

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/ILivingEntityExtensionMixin.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/ILivingEntityExtensionMixin.java
@@ -25,6 +25,8 @@ public abstract class ILivingEntityExtensionMixin {
         LivingEntity self = (LivingEntity) (Object) this;
 
         if (self instanceof Player player) {
+            // TODO :: early return needed if already false? since this may change that to true
+            //         if not (in case we allow players to apply negative bonus, also don't early-return for "can swim in"?
             return SwimData.getData(player).getMaxOxygen(player, fluid) != SwimData.UNLIMITED_OXYGEN;
         }
 
