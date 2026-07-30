7bcca1e1d2f97ab28f2e18c9cde99b36db989bdd
Cadentem <kai.zahn@yahoo.de>
2026-07-30 23:13:20 +0200

    check current mana for ability reservation

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/network/magic/SyncDisableAbility.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/network/magic/SyncDisableAbility.java
@@ -2,12 +2,15 @@ package by.dragonsurvivalteam.dragonsurvival.network.magic;
 
 import by.dragonsurvivalteam.dragonsurvival.DragonSurvival;
 import by.dragonsurvivalteam.dragonsurvival.registry.attachments.MagicData;
+import by.dragonsurvivalteam.dragonsurvival.registry.datagen.Translation;
 import by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.DragonAbility;
 import by.dragonsurvivalteam.dragonsurvival.registry.dragon.ability.DragonAbilityInstance;
+import by.dragonsurvivalteam.dragonsurvival.util.DSColors;
 import net.minecraft.network.FriendlyByteBuf;
 import by.dragonsurvivalteam.dragonsurvival.network.codec.ByteBufCodecs;
 import by.dragonsurvivalteam.dragonsurvival.network.codec.StreamCodec;
 import by.dragonsurvivalteam.dragonsurvival.network.compat.CustomPacketPayload;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import by.dragonsurvivalteam.dragonsurvival.network.compat.PayloadContext;
 import org.jetbrains.annotations.NotNull;
@@ -22,17 +25,29 @@ public record SyncDisableAbility(ResourceKey<DragonAbility> ability, boolean isD
             SyncDisableAbility::new
     );
 
+    @Translation(comments = "You do not have enough mana for the reservation cost of this ability")
+    private static final String NOT_ENOUGH_MANA = Translation.Type.GUI.wrap("message.not_enough_mana_for_reserve");
+
     public static void handleServer(final SyncDisableAbility packet, final PayloadContext context) {
         context.enqueueWork(() -> {
-            MagicData data = MagicData.getData(context.player());
-            DragonAbilityInstance ability = data.getAbility(packet.ability());
+            MagicData magic = MagicData.getData(context.player());
+            DragonAbilityInstance ability = magic.getAbility(packet.ability());
 
             if (ability == null) {
                 return;
             }
 
-            if (packet.isDisabled() && ability.isApplyingEffects() && ability == data.getCurrentlyCasting()) {
-                data.stopCasting(context.player(), ability, true);
+            if (packet.isDisabled() && ability.isApplyingEffects() && ability == magic.getCurrentlyCasting()) {
+                magic.stopCasting(context.player(), ability, true);
+            }
+
+            if (!packet.isDisabled() && packet.isManual()) {
+                if (ability.cannotReserve(context.player())) {
+                    // Automatic disable is already handled in the 'tick' method of the ability instance
+                    context.reply(new SyncDisableAbility(packet.ability(), true, true));
+                    context.player().sendSystemMessage(Component.translatable(NOT_ENOUGH_MANA).withColor(DSColors.RED));
+                    return;
+                }
             }
 
             ability.setDisabled(context.player(), packet.isDisabled(), packet.isManual());
