176aaf8c61bc096b779b73a03cbd4f635727b15b
seelderr <apersonwhoexistsinemail@gmail.com>
2026-08-03 21:11:24 -0700

    Moved charged coal recipe changes from a hand authored override back to datagen

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/datagen/DSRecipes.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/registry/datagen/DSRecipes.java
@@ -67,7 +67,7 @@ public class DSRecipes extends RecipeProvider {
                 .unlockedBy(getHasName(DSItems.ELDER_DRAGON_DUST.value()), has(DSItems.ELDER_DRAGON_DUST.value()))
                 .save(output);
 
-        ShapedRecipeBuilder.shaped(RecipeCategory.FOOD, DSItems.CHARGED_COAL.value(), 4)
+        ShapedRecipeBuilder.shaped(RecipeCategory.FOOD, DSItems.CHARGED_COAL.value(), 3)
                 .pattern("RRR")
                 .pattern("CCR")
                 .pattern("CCR")
