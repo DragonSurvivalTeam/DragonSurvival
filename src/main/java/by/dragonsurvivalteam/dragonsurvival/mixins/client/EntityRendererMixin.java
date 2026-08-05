8680e2c2de85c817aaed6a6b50989c3b503a4b1e
seelderr <apersonwhoexistsinemail@gmail.com>
2026-08-04 22:24:02 -0700

    Increase the culling bounds for dragon players to prevent overly aggressive culling

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/client/EntityRendererMixin.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/client/EntityRendererMixin.java
@@ -2,20 +2,27 @@ package by.dragonsurvivalteam.dragonsurvival.mixins.client;
 
 import by.dragonsurvivalteam.dragonsurvival.client.render.entity.PillageIconRenderer;
 import by.dragonsurvivalteam.dragonsurvival.client.render.entity.SmeltEffectIconRenderer;
+import by.dragonsurvivalteam.dragonsurvival.common.capability.DragonStateProvider;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderer.class)
 public abstract class EntityRendererMixin<T extends Entity> {
+    @Unique private static final double dragonSurvival$MODEL_CULLING_PADDING = 10.0D;
+
     @Shadow @Final protected EntityRenderDispatcher entityRenderDispatcher;
 
     @Inject(method = "render", at = @At("HEAD"))
@@ -23,5 +30,14 @@ public abstract class EntityRendererMixin<T extends Entity> {
         // If we try to render in a "normal" way through a layer there will be weird left-over pose stack modifications (rotation, position etc.), messing with our icon
         PillageIconRenderer.renderIcon(entity, poseStack, entityRenderDispatcher.distanceToSqr(entity));
         SmeltEffectIconRenderer.renderIcon(entity, poseStack, entityRenderDispatcher.distanceToSqr(entity));
+    }
+
+    @ModifyReturnValue(method = "getBoundingBoxForCulling", at = @At("RETURN"))
+    private AABB dragonSurvival$expandDragonCullingBounds(final AABB original, final T entity) {
+        if (entity instanceof Player player && DragonStateProvider.isDragon(player)) {
+            return original.inflate(dragonSurvival$MODEL_CULLING_PADDING * player.getScale());
+        }
+
+        return original;
     }
 }
