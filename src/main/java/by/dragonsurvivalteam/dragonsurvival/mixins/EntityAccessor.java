91bff884de9bb7cd98d101cd7b17d03ef9215a7e
seelderr <apersonwhoexistsinemail@gmail.com>
2026-08-15 10:54:57 -0700

    Added a command to debug dragon riding

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/EntityAccessor.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/EntityAccessor.java
@@ -4,6 +4,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityDimensions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(Entity.class)
 public interface EntityAccessor {
@@ -12,4 +13,13 @@ public interface EntityAccessor {
 
     @Accessor("dimensions")
     EntityDimensions dragonSurvival$getDimensions();
+
+    @Accessor("vehicle")
+    void dragonSurvival$setVehicle(Entity vehicle);
+
+    @Invoker("addPassenger")
+    void dragonSurvival$addPassenger(Entity passenger);
+
+    @Invoker("removePassenger")
+    void dragonSurvival$removePassenger(Entity passenger);
 }
