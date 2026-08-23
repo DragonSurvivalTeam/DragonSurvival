c148902593c23649fb3292f1e02897b801f3e02d
Cadentem <kai.zahn@yahoo.de>
2026-08-23 19:45:57 +0200

    add null check for minecolonies

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/IBlocksExtensionMixin.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/mixins/IBlocksExtensionMixin.java
@@ -4,14 +4,12 @@ import by.dragonsurvivalteam.dragonsurvival.common.handlers.magic.ClimbingHandle
 import by.dragonsurvivalteam.dragonsurvival.registry.attachments.AttachmentManager;
 import by.dragonsurvivalteam.dragonsurvival.registry.attachments.ClimbableData;
 import by.dragonsurvivalteam.dragonsurvival.registry.attachments.DSDataAttachments;
-import net.minecraft.core.BlockPos;
-import net.minecraft.tags.BlockTags;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.extensions.IForgeBlock;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = IForgeBlock.class, remap = false)
 public interface IBlocksExtensionMixin {
@@ -19,16 +17,17 @@ public interface IBlocksExtensionMixin {
      * @author Dragon Survival Team
      * @reason Extends Forge's default ladder check with Dragon Survival climbable effects.
      */
-    @Overwrite(remap = false)
-    default boolean isLadder(final BlockState state, final LevelReader level, final BlockPos position, final LivingEntity entity) {
-        if (state.is(BlockTags.CLIMBABLE) || entity == null) {
-            return state.is(BlockTags.CLIMBABLE);
+    @ModifyReturnValue(method = "isLadder", at = @At("RETURN"))
+    default boolean isLadder(boolean original, @Local(argsOnly = true) final LivingEntity entity) {
+        if (entity == null) {
+            // MineColonies may cause this to be null
+            return original;
         }
 
         ClimbableData data = AttachmentManager.getExistingData(entity, DSDataAttachments.CLIMBABLE_DATA).orElse(null);
 
-        if (data == null) {
-            return false;
+        if (data == null || original) {
+            return original;
         }
 
         return ClimbingHandler.canClimb(entity, data);
