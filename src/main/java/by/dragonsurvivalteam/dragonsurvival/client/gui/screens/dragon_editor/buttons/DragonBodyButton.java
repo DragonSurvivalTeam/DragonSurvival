5a80887278c73cc483dffee882195e1c88e03ec6
seelderr <apersonwhoexistsinemail@gmail.com>
2026-08-29 11:04:20 -0700

    Improved positioning of the body icon tooltip

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/client/gui/screens/dragon_editor/buttons/DragonBodyButton.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/client/gui/screens/dragon_editor/buttons/DragonBodyButton.java
@@ -14,7 +14,9 @@ import by.dragonsurvivalteam.dragonsurvival.util.DragonUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.navigation.ScreenRectangle;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.tooltip.MenuTooltipPositioner;
 import net.minecraft.core.Holder;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
@@ -166,7 +168,11 @@ public class DragonBodyButton extends ExtendedButton implements HoverDisableable
                     setTooltip(Tooltip.create(Component.translatable(NOT_UNLOCKED)));
                 }
             } else if (isHovered()) {
-                Minecraft.getInstance().screen.setTooltipForNextRenderPass(tooltip);
+                Minecraft.getInstance().screen.setTooltipForNextRenderPass(
+                        tooltip,
+                        new MenuTooltipPositioner(new ScreenRectangle(getX(), getY(), width, height)),
+                        false
+                );
             }
         }
 
