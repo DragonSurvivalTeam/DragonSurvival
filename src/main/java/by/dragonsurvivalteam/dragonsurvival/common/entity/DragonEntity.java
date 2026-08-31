ebd5b9ef98fdd3b095d2b147dcab07c5f4533524
Cadentem <kai.zahn@yahoo.de>
2026-08-31 17:11:22 +0200

    adjust fly idle movement check to match 26.1.2

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/entity/DragonEntity.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/entity/DragonEntity.java
@@ -672,7 +672,7 @@ public class DragonEntity extends LivingEntity implements GeoEntity {
                 } else if (ServerFlightHandler.isSpin(player)) {
                     state.setAnimation(DragonAnimations.FLY_SPIN.getAnimation());
                     animationController.transitionLength(2);
-                } else if (deltaMovement.length() < 4 && AnimationUtils.doesAnimationExist(DRAGON_MODEL, this, DragonAnimations.FLY_IDLE.getAnimation())) {
+                } else if (deltaMovement.length() < 0.3 && AnimationUtils.doesAnimationExist(DRAGON_MODEL, this, DragonAnimations.FLY_IDLE.getAnimation())) {
                     state.setAnimation(DragonAnimations.FLY_IDLE.getAnimation());
                     animationController.transitionLength(2);
                 } else {
