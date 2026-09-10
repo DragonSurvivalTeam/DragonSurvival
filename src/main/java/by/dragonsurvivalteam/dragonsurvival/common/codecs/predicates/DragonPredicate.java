50a08a7c7b06f9730e299e9d7ddfe372f17e2c98
seelderr <apersonwhoexistsinemail@gmail.com>
2026-09-09 21:19:37 -0700

    Consider previously completed stages when a stage predicate uses a percentage

--- src/main/java/by/dragonsurvivalteam/dragonsurvival/common/codecs/predicates/DragonPredicate.java
+++ src/main/java/by/dragonsurvivalteam/dragonsurvival/common/codecs/predicates/DragonPredicate.java
@@ -75,7 +75,7 @@ public record DragonPredicate(
             return false;
         }
 
-        if (dragonStage.isPresent() && !dragonStage.get().matches(handler.stage(), handler.getGrowth())) {
+        if (dragonStage.isPresent() && !dragonStage.get().matches(handler.stage(), handler.getGrowth(), handler.species().value().getStages(level.registryAccess()))) {
             return false;
         }
 
